98e661a1be61a6c4d7cfbf22aea40962bff7a239
Bhavesh <bhavesh39shinde@gmail.com>
2025-04-18 16:24:08 +0530

    Resolved merge conflict in README.md

--- src/server.js
+++ src/server.js
@@ -1,28 +1,46 @@
 //src/server.js
-
 const http = require('http');
 const Router = require('./router');
 
-class Server{
-    constructor(){
+class Server {
+    constructor() {
         this.router = new Router();
     }
 
-    get(path,handler){
-        this.router.get(path,handler)
+    get(path, handler) {
+        this.router.get(path, handler);
     }
 
-    post(path,handler){
-        this.router.post(path,handler)
+    post(path, handler) {
+        this.router.post(path, handler);
     }
 
-    listen(port,callback){
-        const server = http.createServer((req,res)=>{
-            this.router.handle(req,res);
+    put(path, handler) {
+        this.router.put(path, handler);
+    }
+
+    patch(path, handler) {
+        this.router.patch(path, handler);
+    }
+
+    delete(path, handler) {
+        this.router.delete(path, handler);
+    }
+
+    head(path, handler) {
+        this.router.head(path, handler);
+    }
+
+    options(path, handler) {
+        this.router.options(path, handler);
+    }
+
+    listen(port, callback) {
+        const server = http.createServer((req, res) => {
+            this.router.handle(req, res);
         });
-        server.listen(port,callback)
+        server.listen(port, callback);
     }
-
 }
 
-module.exports = () => new Server()
+module.exports = () => new Server();
